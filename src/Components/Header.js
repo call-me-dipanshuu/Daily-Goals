06c62b00548f635712ac536e86d9c9b52c3c3dd9
call-me-dipanshuu <imdipanshuu@gmail.com>
2022-11-03 13:32:20 +0530

    first done

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <>
+      <nav>Get Ready to Complete Your Daily Goals</nav>;
+      <div className="header">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </>
+  );
+};
+
+export default Header;
